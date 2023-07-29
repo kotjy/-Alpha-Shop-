840d472a80555ec7d199ad5318863ec73d56e49d
jendu <maggie891209@gmail.com>
2023-07-29 23:22:16 +0800

    no message

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,34 +1,30 @@
+import styles from './Main.module.css'
+import StepProgress from "./StepProgress/StepProgress";
+import StepOne from "../Main/Steps/StepOne/StepOne"
+import StepTwo from "../Main/Steps/StepTwo/StepTwo"
+import StepThree from "../Main/Steps/StepThree/StepThree"
+import ProgressControl from './ProgressControl/ProgressControl';
+import Cart from './Cart/Cart';
+import { useState } from 'react';
 
-import StepProgress from "./StepProgress/StepProgress.jsx";
-import StepOne from "./Steps/StepOne/StepOne";
-import ProgressControl from "./ProgressControl/ProgressControl.jsx";
-//import StepTwo from "./components/Main/Steps/StepTwo/StepTwo.jsx";
-//import StepThree from "./components/Main/Steps/StepThree/StepThree.jsx";
-import Cart from "./Cart/Cart.jsx"
-import styles from './Main.module.css';
+export default function Step(){
+    const [page, setPage] = useState(1)
 
-export default function App() {
-  return (
-
-   <main className ={styles.mainContainer}>
-
-    <div className ={styles.registerContainer}>
-       <StepProgress />
-    <div className={styles.formContainer}>
-      <StepOne />
-      { /*<StepTwo />
-      <StepThree /> */}
-    </div>
-    <div className={styles.cartContainer}>
-      < Cart />
-    </div>
-   
-   <div className={styles.controlContainer}>
-    <ProgressControl />
-   </div>
-    </div>
-   </main>    
-  );
-    
+    return(
+        <section className={styles.container}>
+            <div className={styles.leftScreen}>
+            <h2 className={styles.title}>結帳</h2>
+            <div className={styles.progress}><StepProgress page={page}/></div>
+            <div className={styles.step}>
+                {page === 1 && <StepOne />}
+                {page === 2 && <StepTwo />}
+                {page === 3 && <StepThree />}
+            </div>
+            <div className={styles.progress_control}><ProgressControl page={page} setPage={setPage}/></div>
+            </div>
+            <div className={styles.rightScreen}>
+             <Cart />
+            </div>
+        </section>
+    )
 }
-
