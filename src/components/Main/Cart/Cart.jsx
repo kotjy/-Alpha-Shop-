522575a8640e20837f53f0007d5ce5cb4cc213b7
jendu <maggie891209@gmail.com>
2023-08-04 16:09:25 +0800

    優化

--- src/components/Main/Cart/Cart.jsx
+++ src/components/Main/Cart/Cart.jsx
@@ -1,49 +1,8 @@
 import React, { useState } from "react";
-import { dummyProducts } from "./itemData.jsx";
-import { ReactComponent as IconPlus } from '../../../icons/plus.svg'
-import { ReactComponent as IconMinus } from '../../../icons/minus.svg'
+import { dummyProducts } from "./itemData.js";
 import styles from "./Cart.module.css"
+import CartItem from "./CartItem.jsx";
 
-
-function CartItems({ products, onPlus, onMinus }) {
-  return (
-    <>
-      {products.map((product) => (
-        <div key={product.id} id={product.id} className={styles.productContainer}>
-        <img className={styles.imgContainer} src={product.img} alt={product.name} />
-          <div className={styles.productInfo}>
-        
-            <div className={styles.productName}>{product.name}</div>
-            <div className={styles.productControlContainer}>
-              <div className={styles.productControl}>
-                <svg
-                  className={styles.minus}
-                  onClick={() => {
-                    onMinus(product.id);
-                  }}
-                >
-                  <IconMinus />
-                </svg>
-                <span className={styles.productCount}>{product.quantity}</span>
-                <svg
-                  className={styles.plus}
-                  onClick={() => {
-                    onPlus(product.id);
-                  }}
-                >
-                  <IconPlus />
-                </svg>
-              </div>
-            </div>
-            <div className={styles.price}>
-              ${(product.price * product.quantity).toLocaleString()}
-            </div>
-          </div>
-        </div>
-      ))}
-    </>
-  );
-} 
 
 const Cart = () => {
   const [products, setProducts] = useState([...dummyProducts]);
@@ -84,14 +43,18 @@ const Cart = () => {
   return (
     <div className={styles.cart}>
       <section className={styles.cartContainer}>
-        <h3 className={styles.cardTitle}>購物籃</h3>
+        <h3 className={styles.cartTitle}>購物籃</h3>
 
         <section className={styles.productList}>
-          <CartItems
-            products={products}
+           {products.map((product) => (
+             <CartItem 
+            product={product}
+            key={product.id}
             onPlus={handlePlusClick}
             onMinus={handleMinusClick}
-          />
+            />
+            ))}
+
         </section>
 
         <section className={styles.cartInfo}>
@@ -103,7 +66,8 @@ const Cart = () => {
           <div className={styles.price}>${totalPrice.toLocaleString()}</div>
         </section>
       </section>
-    </div>
+    </div> 
+          
   );
 };
 
